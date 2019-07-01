efc06a8b34fe9d16d7e0095a0cf7b78acc676495
0neBean <pein790751571@aliyun.com>
2019-07-01 10:48:42 +0800

    no message

---
+++ README.md
@@ -0,0 +1,6 @@
+# net.onebean.spring.boot
+onebean 框架spring依赖包
+
+
+
+更多介绍和API请参考这里:[kepler.onebean.net](http://kepler.onebean.net/doc/1.1.0.4.RELEASE/b0004#paragraph1)
---
+++ src/main/java/com/eakay/Test.java
@@ -0,0 +1,10 @@
+package com.eakay;
+
+/**
+ * @author 0neBean
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello 0neBean !");
+    }
+}
